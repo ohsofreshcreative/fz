3bc6e1aa96cad73fed0491cc3d6804c518e6df7e
Arek <kontakt@ohsofresh.pl>
2026-04-10 11:06:20 +0200

    Automatyczny deploy

--- resources/views/blocks/hero.blade.php
+++ resources/views/blocks/hero.blade.php
@@ -81,7 +81,7 @@ $sectionClass .= $flip ? ' order-flip' : '';
                 @foreach($g_hero['certs'] as $cert)
                     @if(!empty($cert['cert_pdf']['url']) && !empty($cert['cert_image']['url']))
                         <a href="{{ $cert['cert_pdf']['url'] }}" target="_blank">
-                            <img src="{{ $cert['cert_image']['url'] }}" alt="{{ $cert['cert_image']['alt'] ?? '' }}"/>
+                            <img class="rounded-full" src="{{ $cert['cert_image']['url'] }}" alt="{{ $cert['cert_image']['alt'] ?? '' }}"/>
                         </a>
                     @endif
                 @endforeach
